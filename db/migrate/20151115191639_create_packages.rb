ccb3419abd883dd31c748af07be8f16d4858eace
bernardAdark <bernard.adarkwah@meltwater.org>
2015-11-16 10:11:21 +0000

    Remove vendor model

--- db/migrate/20151115191639_create_packages.rb
+++ db/migrate/20151115191639_create_packages.rb
@@ -4,8 +4,9 @@ class CreatePackages < ActiveRecord::Migration
       t.string     :tracking_code
       t.integer    :weight_kg
       t.references :user,   index: true, foreign_key: true
-      t.references :vendor, index: true, foreign_key: true
+      t.string     :vendor 		
       t.string     :location
+
 
       t.timestamps null: false
     end
