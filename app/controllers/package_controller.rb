b76e2cdf405803b4a6f52a64f100b786670ba9d0
Ekuma Chidi <bukki.arise@yahoo.co.uk>
2015-11-19 12:47:32 +0000

    Add user_id to packages

---
+++ app/controllers/package_controller.rb
@@ -0,0 +1,49 @@
+class PackageController < ApplicationController
+  before_action :find_package, only: [:show, :destroy, :update, :edit]
+
+  def index
+  	@packages = Package.all
+  	@user = User.find[params(:id])
+  end
+
+  def new
+  	@package = Package.new
+  end
+
+  def create
+  	@package = Package.new(package_param)
+  	@package.user_id = User.find(params[:id])
+  	if @package.save
+  		redirect_to @package
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  	if @package.update(package_param)
+  		redirect_to @package
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+
+  end
+
+  private
+  	def package_param
+  		params.require(:package).permit(:tracking_code, :weight_kg, :vendor, :location)
+  	end
+
+  	def find_package
+  		@package = Package.find(params[:id])
+  	end
+end
