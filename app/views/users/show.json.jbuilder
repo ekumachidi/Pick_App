0a4ec1f0112258af67a77a7b2c17bcdf389bbb6b
Ekuma Chidi <bukki.arise@yahoo.co.uk>
2015-11-19 12:38:06 +0000

    Add User controller and views

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :address, :phone_no, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151118111520) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer  "package_id"
+    t.integer  "courier_id"
+    t.integer  "admin_id"
+    t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "assignments", ["admin_id"], name: "index_assignments_on_admin_id"
+  add_index "assignments", ["courier_id"], name: "index_assignments_on_courier_id"
+  add_index "assignments", ["package_id"], name: "index_assignments_on_package_id"
+
+  create_table "couriers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "packages", force: :cascade do |t|
+    t.string   "tracking_code", null: false
+    t.integer  "weight_kg"
+    t.integer  "user_id",       null: false
+    t.string   "vendor",        null: false
+    t.string   "location",      null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "packages", ["user_id"], name: "index_packages_on_user_id"
+
+  create_table "payments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
