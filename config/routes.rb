a1bb7997e1174f9a3e66a24d91079f90ed9484a6
Ekuma Chidi <bukki.arise@yahoo.co.uk>
2015-11-19 03:52:08 +0000

    Create package controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'package/index'
+
+  get 'package/new'
+
+  get 'package/edit'
+
+  get 'package/show'
+
   resources :couriers
+  resources :packages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
